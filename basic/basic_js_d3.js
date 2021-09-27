0c0bd23bc06443ca7efd2d359cd970e06e2d5f7f
Henry <h.ngosytchev@gmail.com>
2021-09-27 23:05:49 +0200

    basic file cleaned

--- basic/basic_js_d3.js
+++ basic/basic_js_d3.js
@@ -1,44 +1,44 @@
 //--- BASIC 1
 //solution1
 
-// var randomNum = Math.floor(Math.random() * 31 - 5);
-// document.write(randomNum);
+var randomN = Math.floor(Math.random() * 31 - 5);
+document.write(randomN);
 
-// if (randomNum <= 10) {
-//     console.log("The weather is cold");
-// } else {
-//     console.log("The weather is moderate");
-// }
+if (randomN <= 10) {
+    console.log("The weather is cold");
+} else {
+    console.log("The weather is moderate");
+}
 
 // //solution 2
-// var randomNum = Math.floor(Math.random() * 31 - 5);
-// document.write(randomNum);
+var randomN = Math.floor(Math.random() * 31 - 5);
+document.write(randomN);
 
-// if (randomNum > 10) {
-//     console.log("The weather is moderate");
-// } else {
-//     console.log("The weather is cold");
-// }
+if (randomN > 10) {
+    console.log("The weather is moderate");
+} else {
+    console.log("The weather is cold");
+}
 
 
-// //--- BASIC2
-// // Solution 1
-// var myArray = [1, 7, -3, 9];
-// document.write(myArray.sort(function(a, b) { return a - b })[3] +
-//     "<br>");
+//--- BASIC2
+// Solution 1
+var myArray = [1, 7, -3, 9];
+document.write(myArray.sort(function(a, b) { return a - b })[3] +
+    "<br>");
 
-// //Solution 2
-// var myArray = [1, 7, -3, 9];
-// var newArray = myArray.sort(function(a, b) { return a - b });
-// console.log(newArray[newArray.length - 1]);
+//Solution 2
+var myArray_2 = [1, 7, -3, 9];
+var newArray = myArray_2.sort(function(a, b) { return a - b });
+console.log(newArray[newArray.length - 1]);
 
-// //Solution 3
-// var myArray = [1, 7, -3, 9];
-// console.log(
-//     myArray.sort(function(a, b) { return a - b }).reverse()[0]
-// );
+//Solution 3
+var myArray_3 = [1, 7, -3, 9];
+console.log(
+    myArray_3.sort(function(a, b) { return a - b }).reverse()[0]
+);
 
-// // Solution 4
+// Solution 4
 // for (i = 0; i < number.length; i++) {
 
 //     var max1 = number[i + 1];
@@ -49,10 +49,9 @@
 // }
 
 // //not allowed :D
-// var myArray = [1, 7, -3, 9];
-// let x = Math.max.apply(null, myArray);;
-// document.write(x);
-// console.log(max);
+var myArray_4 = [1, 7, -3, 9];
+let x = Math.max.apply(null, myArray_4);;
+console.log(x);
 
 
 // ----------------   FROM HENRY   ----------------
@@ -79,13 +78,12 @@ if (randomNumber < -5) {
 
 
 //BASIC3
+var randomNbr = Math.floor(Math.random() * 31 - 5);
+console.log(randomNbr);
 
-var randomNum = Math.floor(Math.random() * 31 - 5);
-console.log(randomNum);
-
-if (randomNum < 15) {
+if (randomNbr < 15) {
     document.getElementsByClassName("document")[0].innerHTML = "It's cold!";
-} else if (randomNum >= 15, randomNum < 20) {
+} else if (randomNbr >= 15, randomNbr < 20) {
     document.getElementsByClassName("document")[0].innerHTML = "It's warm";
 } else {
     document.getElementsByClassName("document")[0].innerHTML = "It's hot!";
